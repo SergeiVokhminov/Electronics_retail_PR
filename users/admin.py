98cfbca218a1b3817091c95903eade15884ec1a8
sergei_vokhminov <rreaper@list.ru>
2025-05-14 20:58:11 +0300

    корректировка модуля admin приложения users для отображения в админ-панели

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from users.models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    """Настройки отображения модели User в админ-панели Django."""
+
+    list_display = ("id", "email", "avatar", "is_active")
+    search_fields = ("email",)
